709d1bc8b0fdc8393494a67b951412f63eb588b2
Raphael <raphaelceccato@hotmail.com>
2024-11-08 14:38:02 -0300

    update wp-fav-posts.php: create table on init

--- src/wp-fav-posts.php
+++ src/wp-fav-posts.php
@@ -12,11 +12,15 @@ if (!defined("ABSPATH")) exit;
 define("PLUGIN_DIR", plugin_dir_path(__FILE__));
 
 
+require_once(PLUGIN_DIR . "src/hook_callbacks.php");
+require_once(PLUGIN_DIR . "src/routes.php");
+
+
 
 class WP_Fav_Posts {
     public function __construct() {
-        
-    }    
+        add_action("init", "wpfav_create_table");
+    }
 }
 
 
