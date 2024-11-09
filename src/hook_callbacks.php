d5958c2d2bf6dbf65cb788be2d52f2fe39bdc07f
Raphael <raphaelceccato@hotmail.com>
2024-11-09 10:02:23 -0300

    update hook_callbacks.php: remove wpfav_create_table()

--- src/hook_callbacks.php
+++ src/hook_callbacks.php
@@ -2,27 +2,6 @@
 
 if (!defined("ABSPATH")) exit;
 
-require_once(ABSPATH . "wp-admin/includes/upgrade.php");
-
-
-
-function wpfav_create_table() {
-    global $wpdb;
-
-    $table = $wpdb->prefix . "user_bookmarks";
-    $collation = $wpdb->get_charset_collate();
-
-    $sql = "CREATE TABLE $table (
-        id BIGINT(20) NOT NULL AUTO_INCREMENT,
-        user_id BIGINT(20) NOT NULL,
-        post_id BIGINT(20) NOT NULL,
-        PRIMARY KEY  (id),
-        UNIQUE KEY user_bookmark (user_id, post_id)
-    ) $collation;";
-
-    dbDelta($sql);
-}
-
 
 
 function wpfav_is_authed() {
