4c35da16388d3db9c376177dbeb0d8fd165f7d47
Raphael <raphaelceccato@hotmail.com>
2024-11-09 10:03:09 -0300

    update wp-fav-posts.php: add on_init()

--- src/wp-fav-posts.php
+++ src/wp-fav-posts.php
@@ -14,15 +14,28 @@ define("PLUGIN_DIR", plugin_dir_path(__FILE__));
 
 require_once(PLUGIN_DIR . "src/hook_callbacks.php");
 require_once(PLUGIN_DIR . "src/routes.php");
+require_once(PLUGIN_DIR . "src/db_utils.php");
 
 
 
 class WP_Fav_Posts {
     public function __construct() {
-        add_action("init", "wpfav_create_table");
+        add_action("init", "on_init");
+    }
+
+
+    public function on_init() {
+        Database::createTable();
+        
+        register_rest_route("wpfav/v1", "/add-bookmark/(?P<id>\d+)", [
+            "methods"  => "POST",
+            "callback" => "wpfav_add_bookmark",
+            "permission_callback" => "wpfav_is_authed"
+        ]);
     }
 }
 
 
 
 new WP_Fav_Posts();
+
